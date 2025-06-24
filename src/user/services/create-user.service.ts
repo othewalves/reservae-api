e2adc301bdf4e5202f2c16620d59182959182a54
Matheus Alves <matheusfrancisco0330@gmail.com>
2025-06-23 22:07:32 -0300

    feat: encrypted password

--- src/user/services/create-user.service.ts
+++ src/user/services/create-user.service.ts
@@ -2,6 +2,8 @@ import prisma from "../../prisma/client";
 import { ExceptionError } from "../../utils/exception-error";
 import { CreateUserDTO } from "../dtos/create-user.dto";
 
+import { hash } from 'bcryptjs';
+
 export const CreateUserService = async (data: CreateUserDTO) => {
 
     const userAlreadyExists = await prisma.user.findFirst({
@@ -14,6 +16,18 @@ export const CreateUserService = async (data: CreateUserDTO) => {
         throw new ExceptionError('E-mail já cadastrado', 409, 'email')
     }
 
-    const user = await prisma.user.create({ data });
+    const passwordEncrypted = await hash(data.password, 8);
+
+    const user = await prisma.user.create({
+        data: {
+            ...data,
+            password: passwordEncrypted
+        }, select: {
+            email: true,
+            name: true,
+            id: true
+        }
+    });
+
     return user;
 }
